08029b5515afcd238385a0644688849db777cc1e
JessieLDavis <jldavis.animation@gmail.com>
2026-02-19 00:20:48 -0600

    ship updates

---
+++ assets/ship_types.py
@@ -0,0 +1,13 @@
+from ..player.ship_class import PostalShip
+import json
+
+def make_types(cost_range:list=[100,200],fuel_range:list=[100,200],quality:int=0):
+    pass
+
+def create_templates(level:int=1):
+    fuel_max = 100
+    fuel_consumption_rate = 3
+    min_inv = 50
+    max_inv = 100
+    cost = 200
+
