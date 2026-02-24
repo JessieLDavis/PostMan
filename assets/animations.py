07c64a9f6ce5729cd6d4eef28ddd13b3eaccc61b
JessieLDavis <jldavis.animation@gmail.com>
2026-02-24 00:20:27 -0600

    testing animations

--- assets/animations.py
+++ assets/animations.py
@@ -1,6 +1,20 @@
-
+import time
 def launching():
-    pass
+    for x in range(10):
+        if x%3 == 0:
+            print(' '*50)
+            print("      .   "*5)
+            print(' '*50)
+            print("  *       "*6)
+        elif x%4 == 0:
+            print("  '    "*9)
+            # print("     *    "*5)
+        else:
+            print("   '      "*4)
+            print(' '*50)
+            print("       .  "*6)
+            print(' '*50)
+        time.sleep(.1)
 
 def landing():
     pass
@@ -13,3 +27,22 @@ def unload_cargo():
 
 def load_cargo():
     pass
+
+def test_animations():
+
+    print('Launching:')
+    launching()
+    input('---'*10)
+    print('Landing:')
+    landing()
+    input('---'*10)
+    print('Unloading:')
+    unload_cargo()
+    input('---'*10)
+    print('Loading:')
+    load_cargo()
+    input('---'*10)
+
+    return
+
+test_animations()
